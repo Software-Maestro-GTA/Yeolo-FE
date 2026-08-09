b8b49ec1c2ffe653e0a31cd65dc28d72b41f575d
kimseongyu <kskksk0925@naver.com>
2026-08-10 00:20:20 +0900

    remove: 프로필 연관 미사용 컴포넌트 6종 삭제 #63
    - ConfirmModal, ProfileEditModal, ProfileHeader, SettingsSection, TasteAnalysisCard, WithdrawModal 삭제
    - packages/app/src/components/profile/index.ts 내보내기 정리

--- packages/app/src/components/profile/index.ts
+++ packages/app/src/components/profile/index.ts
@@ -1,11 +1,5 @@
 /**
  * @file index.ts
- * @description Export all profile UI components using clean named exports.
+ * @description Export profile UI components using clean named exports.
  */
-export * from './ConfirmModal';
-export * from './ProfileHeader';
-export * from './SettingsSection';
-export * from './TasteAnalysisCard';
 export * from './TermsModal';
-export * from './WithdrawModal';
-export * from './ProfileEditModal';
